0fe1f44b214d0ea7fcececc7fbe2e40d7582611c
Adriano Nascimento <Adriano Nascimento@WAR-MACHINE>
2017-05-30 19:43:52 -0300

    Add project files.

---
+++ WebApi/Models/Usuario.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApi.Models
+{
+    public class Usuario
+    {
+
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public decimal salario { get; set; }
+        public byte[] Foto { get; set; }
+
+        public Usuario()
+        {
+
+        }
+
+    }
+
+}
